209191a87911383a73420ee9c577c05be4869af4
Dwi Hujianto <dwi.hujianto@gmail.com>
2020-06-16 14:44:23 +0700

    init

---
+++ app.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"api/app/handler"
+	"api/app/model"
+	"api/config"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+type App struct {
+	Router *mux.Router
+	DB *gorm.DB
+}
+
+func (a *App) Initialize(config *config.Config) {
+	dbURI := fmt.Sprintf("%s:%s@/%s?charset=%s&parseTime=True",
+		config.DB.Username,
+		config.DB.Passport,
+		config.DB.Name,
+		config.DB.Charset
+	)
+
+	db, err := gorm.Open(config.DB.Dialect, dbURI)
+	if err != nil {
+		log.Fatal("Could not connect database")
+	}
+
+	a.DB = model.DBMigrate(db)
+	a.Router = mux.NewRouter()
+	a.setRouters()
+}
+
+func (a *App) setRouters() {
+	a.Get("/employees", a.GetAllEmployees)
+}
+
+func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("GET")
+}
+
+func (a *App) GetAllEmployees(w http.ResponseWriter, r *http.Request) {
+	handler.GetAllEmployees(a.DB, w, r)
+}
+
+func (a *App) Run(host string) {
+	log.Fatal(http.ListenAndServe(host, a.Router))
+}
---
+++ app/handler/employees.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"api/app/model"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+func getAllEmployees(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	employees := []model.Employee{}
+	db.Find(&employees)
+
+	respondJSON(w, http.StatusOK, employees)
+}
